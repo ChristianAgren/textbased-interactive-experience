0707ebcca56e3a8ef3d8f9c0c04b547e7d6c8e53
Christian Ågren <christian.agren@medieinstitutet.se>
2019-10-29 15:14:17 +0100

    Improved functionality for action history

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const inputButton = document.querySelector('button')
 
+/**
+ * Reads users action input
+ * @param {EventListener} e Listens for keypress/buttonclick
+ */
 function inputUserAction(e) {
-    console.log(e);
 
     if (e.charCode === 13 || e.type === 'click') {
 
@@ -12,13 +15,33 @@ function inputUserAction(e) {
         
         userAction.value = ""
         
-        userAction.focus();
+        userAction.focus()
+        updateListElements(userActionInput)
     }
 }
 
-function updateListElements() {
+/**
+ * Function for managing action history
+ * @param {string} assignedAction User inputs assigned action
+ */
 
+function updateListElements(assignedAction) {
+    let getActionsList = document.querySelectorAll('#user-action-list li'),
+        buildActionArray = []
+        
+    for (i = 0; i < getActionsList.length; i++) {
+         buildActionArray[i] = getActionsList[i].innerText
+    }
+    
+    buildActionArray.shift()
+    buildActionArray.push(assignedAction)  
+    
+    for (i = 0; i < getActionsList.length; i++) {
+        getActionsList[i].innerText = buildActionArray[i]
+    }
 }
+
+// Eventlisteners
 
 inputButton.addEventListener('click', inputUserAction)
 window.addEventListener('keypress', inputUserAction)
