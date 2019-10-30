6204f371e430036e804f7cf40b3e93f0074c6b1f
Christian Ågren <christian.agren@medieinstitutet.se>
2019-10-30 21:30:23 +0100

    fixes for player locations

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const   inputButton = document.querySelector('button'),
         player = {
-            location: "basement"
+            location: "basement",
+            acceptedLocations: ['attic', 'library', 'livingroom', 'basement']
         };
 
 /**
- * Reads users action input
+ * Reads users action input and outputs accordingly
  * @param {EventListener} e Listens for keypress/buttonclick
  */
-function inputUserAction(e) {
+function parseUserInput(e) {
 
     if (e.charCode === 13 || e.type === 'click') {
 
         let userAction = document.querySelector('input'),
             userActionInput = userAction.value,
-            noSpaceString = userActionInput.replace(/\s/g, '');
+            noSpaceString = userActionInput.replace(/\s/g, '').toLowerCase(),
+            checkFirstFourLetters = noSpaceString.substring(0, 4),
+            checkStringEnding = noSpaceString.substring(4);
 
         if (noSpaceString === 'instructions') {
             userActionInput += " - Showing instructions"
@@ -23,25 +26,30 @@ function inputUserAction(e) {
             userActionInput += " - Can't help you right now..."
         }
         
-        else if (noSpaceString === 'goto') {
-            userActionInput += ' - Type "goto" and where you want to go. Locations are to the right.' 
-        }
-        
-        else if (noSpaceString === 'gotolivingroom') {
-            updateLocation('living room') 
-            userActionInput += ' - You went to the ' + player.location 
-        }
-        
-        else if (noSpaceString === 'gotolibrary') {
-            userActionInput += ' - You went to the library.' 
-        }
-        
-        else if (noSpaceString === 'gotoattic') {
-            userActionInput += ' - You went to the attic.' 
-        }
-        
-        else if (noSpaceString === 'gotobasement') {
-            userActionInput += ' - You went to the basement.' 
+        else if (checkFirstFourLetters === 'goto') {
+            if (noSpaceString.length === 4) {
+                userActionInput += ' - Type "goto" and where you want to go. Locations are to the right.' 
+            }
+            else if (checkStringEnding === player.location) {
+                userActionInput += ' - You are already there'
+            }
+            else {
+                let checkIfLocation = false
+
+                for (i = 0; i < player.acceptedLocations.length; i++) {
+                    if (checkStringEnding === player.acceptedLocations[i]) {
+                        console.log(checkStringEnding, player.acceptedLocations[i])
+                        checkIfLocation = true
+                    }
+                }
+                if (checkIfLocation) {
+                    updateLocation(checkStringEnding) 
+                    userActionInput += ' - You went to the ' + player.location 
+                }
+                else {
+                    userActionInput += " - That's not a location..."
+                }
+            }
         }
         
         else {
@@ -55,11 +63,14 @@ function inputUserAction(e) {
     }
 }
 
+function movePlayerLocation(noSpaceString, checkFirstFourLetters) {
+    return userinput
+}
+
 /**
  * Function for managing action history
  * @param {string} assignedAction User inputs assigned action
  */
-
 function updateListElements(assignedAction) {
     let getActionsList = document.querySelectorAll('#user-action-list li'),
         buildActionArray = []
@@ -76,14 +87,36 @@ function updateListElements(assignedAction) {
     }
 }
 
+/**
+ * Manages player location and updates it based on input
+ * @param {string} location 
+ */
 function updateLocation(location) {
-    player.location = location
-    console.log(location);
+    const   oldLocation = document.querySelector('.'+player.location),
+            newLocation = document.querySelector('.'+location);
+
+    oldLocation.innerText = capitilizeFirstLetter(player.location)
+    oldLocation.style.backgroundColor = ''
+    oldLocation.style.color = ''
     
+    newLocation.innerText = capitilizeFirstLetter(location) + ' - You are here'
+    newLocation.style.backgroundColor = '#111112E5'
+    newLocation.style.color = 'var(--secondary-color)'
+    
+    player.location = location
+    
+}
+
+/**
+ * Returns a capitilized string
+ * @param {string} string A string that is to be capitilized
+ */ 
+function capitilizeFirstLetter(string) {
+   return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
 
 // Eventlisteners
 
-inputButton.addEventListener('click', inputUserAction)
-window.addEventListener('keypress', inputUserAction)
+inputButton.addEventListener('click', parseUserInput)
+window.addEventListener('keypress', parseUserInput)
